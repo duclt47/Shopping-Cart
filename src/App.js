a3481532fe731e1888f88d2d43346b8892845050
duc.lt <duc.lt@sutrixsolutions.com>
2017-12-12 14:21:56 +0700

    chia component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Footer from './Component/Footer';
+import Header from './Component/Header';
+import Products from './Component/Products';
+import Message from './Component/Message';
+import Cart from './Component/Cart';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <Header />
+        <main id="mainContainer">
+            <div className="container">
+                <Products />
+                <Message />
+                <Cart />
+            </div>
+        </main>
+        <Footer />
+    </div>
     );
   }
 }
